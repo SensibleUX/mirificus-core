bc6eca5cdd1433885b1fc30045e8fbef20e423b0
Scottux <d.scott.carroll@gmail.com>
2013-09-27 14:52:42 -0400

    Improving Documentation

--- src/Mirificus/Core.php
+++ src/Mirificus/Core.php
@@ -1,11 +1,35 @@
 <?php
 
+/**
+ * @package Mirificus
+ */
 namespace Mirificus;
 
+/**
+ * This the the glue beween all Mirificus packages.
+ * @package Mirificus/Core
+ */
 class Core {
+	/**
+	 * The package version.
+	 * @var string
+	 * @todo Make this dynamic.
+	 * @static
+	 * @access protected
+	 */
 	protected static $strVersion = '0.1';
+
+	/**
+	 * Holds a globally accessible DB adapter definition.
+	 * @var array
+	 * @static
+	 */
 	public static $Database = array();
 
+	/**
+	 * Get the version.
+	 * @return string The package version.
+	 */
 	public static function Version(){
 		return self::$strVersion;
 	}
